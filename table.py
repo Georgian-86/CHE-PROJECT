b4af88764ae9865c3b13acc56f35107856ea0711
Georgian-86 <optimus4586prime@gmail.com>
2024-07-05 20:28:23 +0530

    commiting 3 files

---
+++ table.py
@@ -0,0 +1,4 @@
+for x in range (2,3):
+    for i in range (1,11):
+    #for x in range (2):
+        print(x, "*",i,"=",x*i) 
---
+++ tablle.py
@@ -0,0 +1,5 @@
+x=2
+y=0
+while x == 2:
+    while y< 11:
+        print(x,"*",y,"=",x*y)
