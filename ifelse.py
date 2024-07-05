69607444f36040199175af2b691e8679e8070cb1
Georgian-86 <optimus4586prime@gmail.com>
2024-07-05 23:00:09 +0530

    commitiing new branch

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+x=int(input("enter the value of x :"))
+y=int(input("enter the value of y :"))
+
+if x==y:
+    print("x is equal to y")
+else :
+    if x>y :
+        print("x is greater than y")
+    else: 
+        print("x is less than y")
